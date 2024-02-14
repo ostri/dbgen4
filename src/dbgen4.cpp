5b505badccac5bacb84954debcb818f5f841bcd6
ostri <matjaz.ostroversnik@gmail.com>
2024-02-14 23:11:52 +0100

    support for memory leak detection

--- src/dbgen4.cpp
+++ src/dbgen4.cpp
@@ -1,8 +1,20 @@
 
 #include "appl.hpp"
+#ifndef NDEBUG
+#  include <sanitizer/lsan_interface.h>
+#endif
 int main(int argc, char** argv, char** env)
 {
   dbgen4::appl app;
-
-  return app.exec(argc, argv, env);
+  // NOLINT
+  // const int len = 2000;
+  // int*      x   = new int[len]; // NOLINT
+  // x[0]          = 1;            // NOLINT
+  // x[len + 1]    = 1000;         // NOLINT
+  //  delete[] x;                   // NOLINT
+  auto sts = app.exec(argc, argv, env);
+#ifndef NDEBUG
+  __lsan_do_recoverable_leak_check();
+#endif
+  return sts;
 }
